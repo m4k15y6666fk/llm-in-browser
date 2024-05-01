be59d5bc577eb52b5a35ef2a09c8d1307428073d
hiiro <m4k15y6666fk@outlook.jp>
2024-05-02 00:48:30 +0900

    Added modules for some constants.

---
+++ dev/models.js
@@ -0,0 +1,53 @@
+
+export const MODELS = [
+    {
+        lang: 'ja',
+        name: 'japanese-mistral-300m-base',
+        type: 'Q4_K_M',
+        size: '234.80 MB',
+        url: [
+            'https://huggingface.co/afrideva/japanese-mistral-300m-base-GGUF/resolve/main/japanese-mistral-300m-base.q4_k_m.gguf'
+        ],
+        repo: 'https://huggingface.co/afrideva/japanese-mistral-300m-base-GGUF'
+    },
+    {
+        lang: 'ja',
+        name: 'japanese-mistral-300m-instruction',
+        type: 'Q4_K_M',
+        size: '234.80 MB',
+        url: [
+            'https://huggingface.co/afrideva/japanese-mistral-300m-instruction-GGUF/resolve/main/japanese-mistral-300m-instruction.q4_k_m.gguf'
+        ],
+        repo: 'https://huggingface.co/afrideva/japanese-mistral-300m-instruction-GGUF'
+    },
+    {
+        lang: 'ja',
+        name: 'japanese-stablelm-3b-4e1t-base',
+        type: 'Q4_K_M',
+        size: '1.71 GB',
+        url: [
+            'https://huggingface.co/maddes8cht/stabilityai-japanese-stablelm-3b-4e1t-base-gguf/resolve/main/stabilityai-japanese-stablelm-3b-4e1t-base-Q4_K_M.gguf'
+        ],
+        repo: 'https://huggingface.co/maddes8cht/stabilityai-japanese-stablelm-3b-4e1t-base-gguf'
+    },
+    {
+        lang: 'ja',
+        name: 'japanese-stablelm-3b-4e1t-instruct',
+        type: 'Q4_K_M',
+        size: '1.71 GB',
+        url: [
+            'https://huggingface.co/maddes8cht/stabilityai-japanese-stablelm-3b-4e1t-instruct-gguf/resolve/main/stabilityai-japanese-stablelm-3b-4e1t-instruct-Q4_K_M.gguf'
+        ],
+        repo: 'https://huggingface.co/maddes8cht/stabilityai-japanese-stablelm-3b-4e1t-instruct-gguf'
+    },
+    {
+        lang: 'en',
+        name: 'Tinyllama 1.1B Chat v1.0',
+        type: 'Q4_K_M',
+        size: '669.00 MB',
+        url: [
+            'https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf'
+        ],
+        repo: 'https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF'
+    },
+];
---
+++ dev/template.js
@@ -0,0 +1,23 @@
+
+export const TEMPLATES = [
+    {
+        name: 'None',
+        before: '',
+        after: "",
+    },
+    {
+        name: 'User & Assistant',
+        before: 'User: ',
+        after: "\nAssistant:",
+    },
+    {
+        name: 'System & User & Assistant',
+        before: "System:\nUser: ",
+        after: "\nAssistant:",
+    },
+    {
+        name: 'Q & A',
+        before: 'Q: ',
+        after: "\nA:",
+    },
+];
