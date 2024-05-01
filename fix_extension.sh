abdb8903594def491fc8b855c493e56fa74aa07b
hiiro <m4k15y6666fk@outlook.jp>
2024-05-02 00:42:19 +0900

    Fixed script for use of "alias".

--- fix_extension.sh
+++ fix_extension.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if sed --version > /dev/null 2>&1; then
+if command sed --version > /dev/null 2>&1; then
   # GNU sed
-  find ./dev/esm -name "*.js" -exec sed -i -E "s#export (.*) from '\.(.*)';#export \1 from '.\2\.js';#g" {} +;
-  find ./dev/esm -name "*.js" -exec sed -i -E "s#import (.*) from '\.(.*)';#import \1 from '.\2\.js';#g" {} +;
+  command find ./dev/esm -name "*.js" -exec sed -i -E "s#export (.*) from '\.(.*)';#export \1 from '.\2\.js';#g" {} +;
+  command find ./dev/esm -name "*.js" -exec sed -i -E "s#import (.*) from '\.(.*)';#import \1 from '.\2\.js';#g" {} +;
 else
   # BSD sed
-  find ./dev/esm -name "*.js" -exec sed -i '' -E "s#export (.*) from '\.(.*)';#export \1 from '.\2\.js';#g" {} +;
-  find ./dev/esm -name "*.js" -exec sed -i '' -E "s#import (.*) from '\.(.*)';#import \1 from '.\2\.js';#g" {} +;
+  command find ./dev/esm -name "*.js" -exec sed -i '' -E "s#export (.*) from '\.(.*)';#export \1 from '.\2\.js';#g" {} +;
+  command find ./dev/esm -name "*.js" -exec sed -i '' -E "s#import (.*) from '\.(.*)';#import \1 from '.\2\.js';#g" {} +;
 fi
