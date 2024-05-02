0217589181d30fc2a93278c11a3e37dba5542d71
hiiro <m4k15y6666fk@outlook.jp>
2024-05-03 03:27:36 +0900

    Fixed .gitignore to keep required files and directries.

--- fix_extension.sh
+++ fix_extension.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+command cp -u -r ./dev/src/multi-thread/* ./dev/esm/multi-thread/
+command cp -u -r ./dev/src/single-thread/* ./dev/esm/single-thread/
+
 if command sed --version > /dev/null 2>&1; then
   # GNU sed
   command find ./dev/esm -name "*.js" -exec sed -i -E "s#export (.*) from '\.(.*)';#export \1 from '.\2\.js';#g" {} +;
